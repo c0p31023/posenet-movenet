5f80f1b8c5cd2e7fc9e29f73380a6574af6536fe
c0p31023 <71868529+c0p31023@users.noreply.github.com>
2022-06-26 02:16:39 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,62 +1,47 @@
 
-let o_img;
-let img;
+let video;
 let poseNet;
 let poses = [];
 
 function setup() {
-    
-    createCanvas(640, 480);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
 
-    webcam_output = createCapture(VIDEO);
-    webcam_output.size(width, height);
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
 
-    poseNet = ml5.poseNet(webcam_output, function(){
-        select("#status").html("モデルを読み込みました")
-    });
-
-    webcam_output.hide();
+function modelReady() {
+  select('#status').html('Model Loaded');
 }
 
 function draw() {
-    image(webcam_output, 0, 0,width, height);
+  image(video, 0, 0, width, height);
 
-   // drawKeypoints();
-   // drawSkeleton();
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
 }
 
-function drawKeypoints() {
-    for (let i = 0; i < poses.length; i++) {
-      
-        let pose = poses[i].pose;
-        for (let j = 0; j < pose.keypoints.length; j++) {
-
-            let point = pose.keypoints[j];
-            if (point.score > 0.2) {
-                
-                FileList(0,0,255);
-                noStroke();
-
-                ellipse(point.position.x, point.position.y, 10, 10);
-            }
-            
-        }
-        
-    }
-}
-
-function drawSkeleton() {
-  // Loop through all the skeletons detected
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
   for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke(255, 0, 0);
-      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
     }
-  }
+}
 }
 
 /*
