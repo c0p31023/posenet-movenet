e1b103fbfee0bbeb687eb2773cfe0c8c1f7a89f6
c0p31023 <71868529+c0p31023@users.noreply.github.com>
2022-07-10 21:28:39 +0900

    説明を日本語に訳した

--- sketch.js
+++ sketch.js
@@ -1,56 +1,66 @@
-
+let canvas;
 let video;
 let poseNet;
 let keypoint;
 let poses = [];
 
 function setup() {
-  createCanvas(640, 480);
+  canvas = createCanvas(640, 480);
+  canvas.parent('canvas');
   video = createCapture(VIDEO);
+  video.parent('canvas');
   video.size(width, height);
-
-  // Create a new poseNet method with a single detection
-  poseNet = ml5.poseNet(video, modelReady);
-  // This sets up an event that fills the global variable "poses"
-  // with an array every time new poses are detected
+  // 1つの検出で新しいPoseNetメソッドを作成する
+  poseNet = ml5.poseNet(video);
+  // これは、グローバル変数 "poses" を満たすイベントを設定するものです
+  // 新しいポーズが検出されるたびに，配列で返します
   poseNet.on('pose', function(results) {
     poses = results;
   });
-  // Hide the video element, and just show the canvas
+  // video 要素を非表示にして、canvas だけを表示します
   video.hide();
 }
-
+/*
 function modelReady() {
   select('#status').html('Model Loaded');
 }
-
+*/
 function draw() {
   image(video, 0, 0, width, height);
 
-  // We can call both functions to draw all keypoints and the skeletons
+  // 両方の関数を呼び出すことで、すべてのキーポイントとスケルトンを描画することができます
   drawKeypoints();
   drawSkeleton();
 }
 
-// A function to draw ellipses over the detected keypoints
+// 検出されたキーポイントの上に楕円を描画する関数
 function drawKeypoints()  {
-  // Loop through all the poses detected
+  if (poses.length > 0) {
+  console.log(poses);
+  // 検出されたすべてのポーズをループする
   for (let i = 0; i < poses.length; i++) {
-    // For each pose detected, loop through all the keypoints
+    // 検出された各ポーズに対して、すべてのキーポイントをループする
     let pose = poses[i].pose;
     for (let j = 0; j < pose.keypoints.length; j++) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      // キーポイントは、体の一部を表すオブジェクトです（rightArmやleftShoulderなど）
       keypoint = pose.keypoints[j];
+      // ポーズ確率が0.2より大きい場合のみ楕円を描く
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+     }
     }
-}
-}
+   }
+  }
 
-//to draw the skeletonsfunction
+// 骨格を描画するための関数
 function drawSkeleton() {
-    //Loop through all the skeletons detected
+    // 検出されたすべてのスケルトンをループする
 	for (let i = 0; i < poses.length; i++) {
 		let skeleton = poses[i].skeleton;
-		//For every skeleton, loop through all body connections
+		// すべてのスケルトンについて、すべてのボディ接続をループする
 		for (let j = 0; j < skeleton.length; j++) {
 			let partA = skeleton[j][0];
 			let partB = skeleton[j][1];
@@ -59,23 +69,3 @@ function drawSkeleton() {
 		}
 	}
 }
-
-/*
-
-・使用しているライブラリ
-
-　　p5.min.js　：　アーティスト、デザイナー、教育者、初心者、その他誰にとってもコーディングにアクセスしやすく、包括的にすることに重点を置いた、クリエイティブコーディングのためのJavaScriptライブラリ
-            p5.min.jsの「min」は軽量化されたファイルであることを示している
-	    
- p5.dom.js　：　p5,jsの拡張機能的なライブラリ
-
-　　ml5.min.js　：　アーティストやクリエィティブ・コーダー、学生などの幅広いユーザーが機械学習を親しみやすく触れるられるように作られたライブラリ
-             ml5.min.jsの「min」は軽量化されたファイルであることを示している
-	     
-・関数の呼び出し順（ライブラリから呼び出した関数は一旦考えない）
-
-　　setup()　→　modelReady() → draw() → drawKeypoints() → drawSkeleton()
-  
- 
-
-*/
